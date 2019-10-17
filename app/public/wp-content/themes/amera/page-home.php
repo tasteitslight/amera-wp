0bf438a4932e40978c350ba0f418b222d7725620
willbeing <will.caulfield@gmail.com>
2019-10-17 12:18:48 -0700

    video id

--- app/public/wp-content/themes/amera/page-home.php
+++ app/public/wp-content/themes/amera/page-home.php
@@ -70,8 +70,8 @@ get_header();
 
       <!-- Video  -->
 
-      <section class="container-fluid bg-grey border-top py-5">
-        <div class="container text-center home-video-container">
+      <section id="video" class="container-fluid bg-grey border-top py-5">
+        <div class="container text-center home-video-container mt-5">
         <!--<a id="video-img" onclick="toggle_visibility('video-img');"><img src="<?php bloginfo('template_directory'); ?>/assets/img/general/iso-1.png" width="100%" /></a>-->
         <iframe id="video-iframe" src="https://www.youtube.com/embed/gnoU7EppUAI?rel=0&modestbranding=1&autohide=1&fs=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>  
